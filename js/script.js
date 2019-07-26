d53d3f0c1dfca06d11c3af4c94eb9ed2a5b9e720
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-07-26 15:26:57 +0200

    Add function which used jquery to keep everything in its appropriate way

--- js/script.js
+++ js/script.js
@@ -10,19 +10,82 @@ $(document).ready(function() {
         $(".description-b").hide(500);
         $(".description-c").toggle(500);
           });
+          function myDesign() {
+            var name=document.getElementById("fullName").value;
+            var yourEmail=document.getElementById("myEmail").value;
+            var message=document.getElementById("sms").value;
+            if(name!=" " && yourEmail!="" && message!="" ){
+      confirm(name+", we have received your message. Thank you fro reaching out to us.");
+            }    
+        }
+
+        $(".work-a").hover(function() {
+           $(this).css("transform","scale(1.1)"); 
+         
+
+        },function () {
+            $(this).css("transform","none") 
+            
+         })
+
+
+        $(".work-b").hover(function() {
+            $(".work-a").stop();
+            $(this).css("transform","scale(1.2)"); 
+           
+         },function () {
+            $(this).css("transform","none") 
+         })
+
+
+         $(".work-c").hover(function() {
+            $(".work-b").stop();
+            $(this).css("transform","scale(1.2)"); 
+           
+         },function () {
+            $(this).css("transform","none")  
+         })
+
+
+         $(".work-d").hover(function() {
+            $(this).css("transform","scale(1.2)");
+            $(".work-c").stop(); 
+         },function () {
+            $(this).css("transform","none") 
+         })
+
+
+         $(".work-e").hover(function() {
+            $(this).css("transform","scale(1.2)"); 
+            $(".work-d").stop();
+         },function () {
+            $(this).css("transform","none") 
+         })
+
+
+         $(".work-f").hover(function() {
+            $(this).css("transform","scale(1.2)");
+            $(".work-e").stop(); 
+         },function () {
+            $(this).css("transform","none") 
+         })
+
+
+         $(".work-g").hover(function() {
+            $(this).css("transform","scale(1.2)"); 
+            $(".work-f").stop();
+         },function () {
+            $(this).css("transform","none") 
+         })
+
+
+         $(".work-h").hover(function() {
+            $(".work-g").stop();
+            $(this).css("transform","scale(1.2)"); 
+            
+         },function () {
+            $(this).css("transform","none") 
+         })
   });
-  function myDesign() {
-      var name=document.getElementById("fullName").value;
-      var yourEmail=document.getElementById("myEmail").value;
-      var message=document.getElementById("sms").value;
-      if(name!=" " && yourEmail!="" && message!="" ){
-confirm(name+", we have received your message. Thank you fro reaching out to us.");
-      }
-//    else if (!name.check) {
-       
-//    } 
-       
-  
-     
-  }
+
 
